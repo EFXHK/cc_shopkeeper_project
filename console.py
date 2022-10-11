b1a5a18622dbd5a1e0749aa67678b6b19ac150b1
esau fong <esaufong@gmail.com>
2022-10-11 14:50:43 +0100

    additional database items

--- console.py
+++ console.py
@@ -8,12 +8,19 @@ import repositories.maker_repository as maker_repository
 product_repository.delete_all()
 maker_repository.delete_all()
 
+
 makerX1 = Maker("Siegwards Brewery", "33 Catarina Crescent")
 maker_repository.save(makerX1)
 
+makerX2 = Maker("XXXXX", "3 TTTTTTTTt")
+maker_repository.save(makerX1)
 
 productX1 = Product("Siegbrau", 18, 27, "Comes in a jolly barrel mug", 60, makerX1)
 product_repository.save(productX1)
+
+productX2 = Product("CCCCCCCC", 1, 2, " mug", 6, makerX2)
+product_repository.save(productX2)
+
 
 all_makers = maker_repository.select_all()
 all_products = product_repository.select_all()
@@ -25,3 +32,5 @@ for product in all_products:
     print(product.__dict__)
 
 breakpoint()
+
+#pbd set trace thing 
