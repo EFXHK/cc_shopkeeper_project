425259654864b86542371fb41ef0c0f6b99a5af0
esau fong <esaufong@gmail.com>
2022-10-11 12:12:55 +0100

    makers blueprints setting up

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
 from controllers.product_controller import products_blueprint
-
+from controllers.maker_controller import makers_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(products_blueprint)
+app.register_blueprint(makers_blueprint)
 
 @app.route('/')
 def home():
