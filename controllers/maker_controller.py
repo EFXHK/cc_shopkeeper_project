3d709cf5a4ebf42294804251d079edea415aeb3e
esau fong <esaufong@gmail.com>
2022-10-12 06:30:57 +0100

    added delete route for maker but saving a new maker is still broken

--- controllers/maker_controller.py
+++ controllers/maker_controller.py
@@ -1,7 +1,8 @@
+from itertools import product
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 
-#from models.product import Product
+from models.product import Product
 from models.maker import Maker
 
 import repositories.maker_repository as maker_repository
@@ -22,8 +23,23 @@ def makers():
 @makers_blueprint.route("/makers/new", methods=['GET'])
 def new_maker():
     makers = maker_repository.select_all()
-    ########
-    return render_template("makers/new.html", makers = makers)
+    products = product_repository.select_all()
+    return render_template("makers/new.html", makers = makers, products = products)
+
+# CREATE, POST /makers
+@makers_blueprint.route("/makers", methods=['POST'])
+def create_maker():
+    name = request.form['name'] # this is probably wrong?
+    address = request.form['address'] # i dont understand how to do this one
+    maker_repository.save(makers)
+    return redirect("/makers")
+# do i need to append to a list?
+
+@makers_blueprint.route("makers/<id>/delete", methods=['POST'])
+def delete_maker(id):
+    maker_repository.delete(id)
+    return redirect("/makers")
+
 
 
 
